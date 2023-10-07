05aecf9168e66777321940dfc7eac7a737869684
denis <deniska01.06.01@gmail.com>
2023-10-07 07:49:33 +0400

    v2

--- js/script.js
+++ js/script.js
@@ -1,23 +1,46 @@
 window.addEventListener('DOMContentLoaded',() =>{
-    let video = document.querySelector('#myVideo');
-    let textvideo = document.querySelector('.video-placeholder');
-    let logovideo = document.querySelector('.video-logo');
-    let viewVideo = document.createElement('source');
-    viewVideo.src = textvideo.alt
-  
-    video.classList.add('z50');
-    logovideo.classList.add('z100');
-    video.append(viewVideo)
+
+    if(document.querySelector('.video-section')){
+        let video = document.querySelector('#myVideo');
+        let textvideo = document.querySelector('.video-placeholder');
+        let logovideo = document.querySelector('.video-logo');
+        let viewVideo = document.createElement('source');
+        viewVideo.src = textvideo.alt
+      
+        video.classList.add('z50');
+        logovideo.classList.add('z100');
+        video.append(viewVideo)
+
+    }
+
+    if(document.querySelector('.filter')){
+        let filterOpen = document.querySelector('.filter-text');
+        let filterClose = document.querySelector('.closeFilter');
+        let filterList = document.querySelector('.filter-popup');
+        filterOpen.addEventListener('click', e => {
+            filterList.classList.add('active');
+            filterList.classList.remove('disabled');
+        })
+        filterClose.addEventListener('click', e => {
+            filterList.classList.add('disabled');
+            filterList.classList.remove('active');
+        })
+    }
+
     let noPrev = (e) => {
         e.preventDefault();
     }
     let checklink = e => {
-        if(window.innerWidth <= 768 && !document.querySelector('.nav-burger')){
+        if(!document.querySelector('.nav-burger')){
             let gNav = document.querySelectorAll('.header__nav-link')
+            let subNav = document.querySelector('.submenu').cloneNode(true)
+            console.log(subNav)
             let bNav = document.querySelector('.burger-menu')
             let nBurger = document.createElement('nav')
             nBurger.classList.add('nav-burger')
             bNav.append(nBurger)
+            nBurger.append(subNav)
+            
             for(let i = 0; i < gNav.length; i++){
                 let linkBurger = document.createElement('a')
                 linkBurger.classList.add('burger-link')
@@ -26,39 +49,36 @@ window.addEventListener('DOMContentLoaded',() =>{
                 console.log(linkBurger)
                 nBurger.appendChild(linkBurger)
             }
-            }
-        else{
+      
+        let burger = document.querySelector('.burger')
+        let closeBurger = document.querySelector('.close')
+        burger.addEventListener('click', e => {
+            let bNav = document.querySelector('.burger-menu')
+            let nBurger = document.querySelector('.nav-burger')
+            nBurger.classList.remove('disabled')
+            nBurger.classList.add('active')
+            bNav.classList.remove('disabled')
+            bNav.classList.add('active')
+            burger.classList.add('disabled')
+        })
+        closeBurger.addEventListener('click', e => {
             let bNav = document.querySelector('.burger-menu')
             let nBurger = document.querySelector('.nav-burger')
             nBurger.classList.add('disabled')
             nBurger.classList.remove('active')
             bNav.classList.add('disabled')
-            bNav.classList.remove('active') 
-        }
+            bNav.classList.remove('active')
+            burger.classList.remove('disabled')
+        })
+    
+          }
+          
     }
     if(window.innerWidth <= 768 && !document.querySelector('.nav-burger')){
         checklink()
     }
-    window.addEventListener('resize', checklink) 
+    checklink()
 
-    let burger = document.querySelector('.burger')
-    let closeBurger = document.querySelector('.close')
-    burger.addEventListener('click', e => {
-        let bNav = document.querySelector('.burger-menu')
-        let nBurger = document.querySelector('.nav-burger')
-        nBurger.classList.remove('disabled')
-        nBurger.classList.add('active')
-        bNav.classList.remove('disabled')
-        bNav.classList.add('active')
-        burger.classList.add('disabled')
-    })
-    closeBurger.addEventListener('click', e => {
-        let bNav = document.querySelector('.burger-menu')
-        let nBurger = document.querySelector('.nav-burger')
-        nBurger.classList.add('disabled')
-        nBurger.classList.remove('active')
-        bNav.classList.add('disabled')
-        bNav.classList.remove('active')
-        burger.classList.remove('disabled')
-    })
+        
+   
 }) 
