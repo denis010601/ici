5e9264ffe9f15cc52914f8a429768dc4ad78acf5
denis <deniska01.06.01@gmail.com>
2023-10-13 00:16:18 +0400

    оптимизация модели сайт завршен на 80%

--- icistore/admin.py
+++ icistore/admin.py
@@ -1,15 +1,10 @@
 from django.contrib import admin
-from django.utils.html import format_html
 
-from imagekit.admin import AdminThumbnail
-
-from .models import Category, Sofa, InteriorSofa, Chair, Ottoman, SofaFilter, InteriorSofaFilter, ChairFilter, \
-    OttomanFilter, Material, ImageModel, Gallery
+from .models import Category, Sofa, InteriorSofa, Chair, Ottoman, Material, ImageModel
 
 
 class ImageModelAdmin(admin.ModelAdmin):
     list_display = ['id', 'image']
-
 
 
 class MaterialAdmin(admin.ModelAdmin):
@@ -17,58 +12,28 @@ class MaterialAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 
-class SofaFilterInline(admin.TabularInline):
-    model = SofaFilter
-    extra = 1
-
-
-class InteriorSofaFilterInline(admin.TabularInline):
-    model = InteriorSofaFilter
-    extra = 1
-
-
-class ChairFilterInline(admin.TabularInline):
-    model = ChairFilter
-    extra = 1
-
-
-class OttomanFilterInline(admin.TabularInline):
-    model = OttomanFilter
-    extra = 1
-
-
 class SofaAdmin(admin.ModelAdmin):
-    inlines = [SofaFilterInline]
     list_display = ('title', 'category', 'is_published', 'is_top_product')
 
 
 class InteriorSofaAdmin(admin.ModelAdmin):
-    inlines = [InteriorSofaFilterInline]
     list_display = ('title', 'category', 'is_published', 'is_top_product')
 
 
 class ChairAdmin(admin.ModelAdmin):
-    inlines = [ChairFilterInline]
     list_display = ('title', 'category', 'is_published', 'is_top_product')
 
 
 class OttomanAdmin(admin.ModelAdmin):
-    inlines = [OttomanFilterInline]
     list_display = ('title', 'category', 'is_published', 'is_top_product')
 
-class GalleryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'display_image')
 
-    def display_image(self, obj):
-        return format_html('<img src="{}" style="height:100px;width:100px;"/>'.format(obj.image.url))
+admin.site.register(Category)
+admin.site.register(Material, MaterialAdmin)
 
-    display_image.short_description = 'Image'
-
-admin.site.register(Gallery, GalleryAdmin)
 admin.site.register(ImageModel, ImageModelAdmin)
+
 admin.site.register(Sofa, SofaAdmin)
 admin.site.register(InteriorSofa, InteriorSofaAdmin)
 admin.site.register(Chair, ChairAdmin)
 admin.site.register(Ottoman, OttomanAdmin)
-admin.site.register(Category)
-admin.site.register(Material, MaterialAdmin)
--- icistore/models.py
+++ icistore/models.py
@@ -1,19 +1,10 @@
-import os
 from django.db import models
 from django.utils import timezone
-from imagekit.models import ImageSpecField, ProcessedImageField
-from imagekit.processors import ResizeToFill
 
-class Gallery(models.Model):
-    image = models.ImageField(upload_to='gallery/')
-    class Meta:
-        verbose_name = "Галлерея"
-        verbose_name_plural = "Галлерея"
-    def __str__(self):
-        return f"Image {self.id}"
 
 class ImageModel(models.Model):
     image = models.ImageField(upload_to=f"{timezone.now().year}/{timezone.now().month:02d}/{timezone.now().day:02d}")
+
     class Meta:
         verbose_name = "Картинка"
         verbose_name_plural = "Картинки"
@@ -22,47 +13,54 @@ class ImageModel(models.Model):
         return f"Image {self.pk}"
 
 
-
 class Material(models.Model):
     name = models.CharField(max_length=100)
+
     class Meta:
         verbose_name = "Материал"
         verbose_name_plural = "Материалы"
+
     def __str__(self):
         return self.name
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+
     def __str__(self):
         return self.name
 
-class Sofa(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
-    
+
+class Product(models.Model):
     title = models.CharField(max_length=200, verbose_name="Название")
     subtitle = models.CharField(max_length=200, verbose_name="Вид")
+    meta_description = models.TextField(blank=True, null=True, verbose_name="SEO описание")
+    meta_keywords = models.CharField(max_length=255, blank=True, null=True, verbose_name="SEO ключеве слова")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
     materials = models.ManyToManyField(Material, blank=True, verbose_name="Материал ткани")
     manufacturer = models.CharField(max_length=100, blank=True, verbose_name="Производитель")
     country = models.CharField(max_length=100, blank=True, verbose_name="Страна")
     delivery_days = models.IntegerField(default=0, verbose_name="Доставка в днях")
     warranty = models.CharField(max_length=100, blank=True, verbose_name="гарантия")
-    is_published = models.BooleanField(default=True, verbose_name="Опубликован")
-    is_top_product = models.BooleanField(default=False, verbose_name="Популярный")
     price = models.IntegerField(default=False, blank=True, verbose_name="Цена")
     images = models.ManyToManyField(ImageModel, blank=True, verbose_name="Фото")
-    
+    is_published = models.BooleanField(default=True, verbose_name="Опубликован")
+    is_top_product = models.BooleanField(default=False, verbose_name="Популярный")
+
+    def __str__(self):
+        return f'Filter for {self.title}'
+
+
+class Sofa(Product):
     # Другие поля для описания диванов
     class Meta:
         verbose_name = "Диван"
         verbose_name_plural = "Диваны"
-    def __str__(self):
-        return self.name
 
-
-class SofaFilter(models.Model):
     form_choices = [
         ('эркерный', 'Эркерный'),
         ('модульный', 'Модульный'),
@@ -75,39 +73,12 @@ class SofaFilter(models.Model):
         ('2000-3000мм', '2000-3000мм'),
         ('свыше 3000мм', 'Свыше 3000мм'),
     ]
-
-    sofa = models.ForeignKey('Sofa', on_delete=models.CASCADE)
     form = models.CharField(max_length=50, choices=form_choices, verbose_name="Форма")
     size = models.CharField(max_length=50, choices=size_choices, verbose_name="Размер")
     has_sleeper = models.BooleanField(default=False, verbose_name="Спальное место")
 
-    def __str__(self):
-        return f'Filter for {self.sofa.name}'
 
-class InteriorSofa(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
-    
-    title = models.CharField(max_length=200, verbose_name="Название")
-    subtitle = models.CharField(max_length=200, verbose_name="Вид")
-    materials = models.ManyToManyField(Material, blank=True, verbose_name="Материал ткани")
-    manufacturer = models.CharField(max_length=100, blank=True, verbose_name= "Производитель")
-    country = models.CharField(max_length=100, blank=True,verbose_name="Страна")
-    delivery_days = models.IntegerField(default=0, verbose_name="Доставка в днях")
-    warranty = models.CharField(max_length=100, blank=True, verbose_name="гарантия")
-    is_published = models.BooleanField(default=True, verbose_name="Опубликован")
-    is_top_product = models.BooleanField(default=False, verbose_name="Популярный")
-    price = models.IntegerField(default=False, blank=True, verbose_name="Цена")
-    images = models.ManyToManyField(ImageModel, blank=True, verbose_name="Фото")
-    class Meta:
-        verbose_name = "Интерьерный диван"
-        verbose_name_plural = "Интерьерные диваны"
-    # Другие поля для описания интерьерных диванов
-
-    def __str__(self):
-        return self.name
-
-
-class InteriorSofaFilter(models.Model):
+class InteriorSofa(Product):
     appearance_choices = [
         ('с высокой спинкой', 'С высокой спинкой'),
         ('с невысокой спинкой', 'С невысокой спинкой'),
@@ -117,45 +88,43 @@ class InteriorSofaFilter(models.Model):
         ('металл', 'Металл'),
         ('бук', 'Бук'),
     ]
-
-    interior_sofa = models.ForeignKey('InteriorSofa', on_delete=models.CASCADE)
     appearance = models.CharField(max_length=50, choices=appearance_choices, verbose_name="Спинка")
     legs = models.CharField(max_length=50, choices=legs_choices, verbose_name="Материал")
     has_armrests = models.BooleanField(default=False, verbose_name="Спальное место")
 
-    def __str__(self):
-        return f'Filter for {self.interior_sofa.name}'
+    class Meta:
+        verbose_name = "Интерьерный диван"
+        verbose_name_plural = "Интерьерные диваны"
+    # Другие поля для описания интерьерных диванов
 
-class Chair(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
-    
-    title = models.CharField(max_length=200, verbose_name="Название")
-    subtitle = models.CharField(max_length=200, verbose_name="Вид")
-    materials = models.ManyToManyField(Material, blank=True, verbose_name="Материал ткани")
-    manufacturer = models.CharField(max_length=100, blank=True, verbose_name="Производитель")
-    country = models.CharField(max_length=100, blank=True, verbose_name="Страна")
-    delivery_days = models.IntegerField(default=0, verbose_name="Доставка в днях")
-    warranty = models.CharField(max_length=100, blank=True, verbose_name="гарантия")
-    is_published = models.BooleanField(default=True, verbose_name="Опубликован")
-    is_top_product = models.BooleanField(default=False, verbose_name="Популярный")
-    price = models.IntegerField(default=False, blank=True, verbose_name="Цена")
-    images = models.ManyToManyField(ImageModel, blank=True, verbose_name="Фото")
+
+class Chair(Product):
+    appearance_choices = [
+        ('с высокой спинкой', 'С высокой спинкой'),
+        ('с невысокой спинкой', 'С невысокой спинкой'),
+    ]
+
+    form_choices = [
+        ('круглая', 'Круглая'),
+        ('прямоугольная', 'Прямоугольная'),
+    ]
+
+    legs_choices = [
+        ('металл', 'Металл'),
+        ('бук', 'Бук'),
+    ]
+
+    appearance = models.CharField(max_length=50, choices=appearance_choices, verbose_name="Спинка")
+    form = models.CharField(max_length=50, choices=form_choices, verbose_name="Форма")
+    legs = models.CharField(max_length=50, choices=legs_choices, verbose_name="Материал")
 
     class Meta:
         verbose_name = "Кресло"
         verbose_name_plural = "Кресла"
     # Другие поля для описания кресел
 
-    def __str__(self):
-        return self.name
 
-
-class ChairFilter(models.Model):
-    appearance_choices = [
-        ('с высокой спинкой', 'С высокой спинкой'),
-        ('с невысокой спинкой', 'С невысокой спинкой'),
-    ]
-
+class Ottoman(Product):
     form_choices = [
         ('круглая', 'Круглая'),
         ('прямоугольная', 'Прямоугольная'),
@@ -166,51 +135,9 @@ class ChairFilter(models.Model):
         ('бук', 'Бук'),
     ]
 
-    chair = models.ForeignKey('Chair', on_delete=models.CASCADE, )
-    appearance = models.CharField(max_length=50, choices=appearance_choices, verbose_name="Спинка")
     form = models.CharField(max_length=50, choices=form_choices, verbose_name="Форма")
     legs = models.CharField(max_length=50, choices=legs_choices, verbose_name="Материал")
 
-    def __str__(self):
-        return f'Filter for {self.chair.name}'
-
-class Ottoman(models.Model):
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="Категория")
-    
-    title = models.CharField(max_length=200, verbose_name="Название")
-    subtitle = models.CharField(max_length=200, verbose_name="Вид")
-    materials = models.ManyToManyField(Material, blank=True, verbose_name="Материал ткани")
-    manufacturer = models.CharField(max_length=100, blank=True, verbose_name="Производитель")
-    country = models.CharField(max_length=100, blank=True, verbose_name="Страна")
-    delivery_days = models.IntegerField(default=0, verbose_name="Доставка в днях")
-    warranty = models.CharField(max_length=100, blank=True, verbose_name="гарантия")
-    is_published = models.BooleanField(default=True, verbose_name="Опубликован")
-    is_top_product = models.BooleanField(default=False, verbose_name="Популярный")
-    price = models.IntegerField(default=False, blank=True, verbose_name="Цена")
-    images = models.ManyToManyField(ImageModel, blank=True, verbose_name="Фото")
     class Meta:
         verbose_name = "Пуф"
         verbose_name_plural = "Пуфы"
-    # Другие поля для описания пуфов
-
-    def __str__(self):
-        return self.name
-
-
-class OttomanFilter(models.Model):
-    form_choices = [
-        ('круглая', 'Круглая'),
-        ('прямоугольная', 'Прямоугольная'),
-    ]
-
-    legs_choices = [
-        ('металл', 'Металл'),
-        ('бук', 'Бук'),
-    ]
-
-    ottoman = models.ForeignKey('Ottoman', on_delete=models.CASCADE)
-    form = models.CharField(max_length=50, choices=form_choices, verbose_name="Форма")
-    legs = models.CharField(max_length=50, choices=legs_choices, verbose_name="Материал")
-
-    def __str__(self):
-        return f'Filter for {self.ottoman.name}'
